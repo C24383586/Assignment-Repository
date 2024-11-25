379581537e31c7911f8e0f0c9091cebcbc61853e
C24383586 <C24383586@myTUDublin.ie>
2024-11-25 14:57:06 +0000

    Commit4

--- music-assignment/audio_demo.gd
+++ music-assignment/audio_demo.gd
@@ -27,4 +27,5 @@ func _on_h_slider_value_changed(value: float) -> void:
 
 
 func _on_h_slider_2_value_changed(value: float) -> void:
+	var f: AudioEffectAmplify 
 	pass # Replace with function body.
